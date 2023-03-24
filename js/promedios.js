14cb7ef0b1861e7c9b35a83afb12d209453d3acb
C0z3e <inbonilla@uniboyaca.edu.co>
2023-03-23 20:45:13 -0500

    Modificaciones en el index y js

---
+++ js/promedios.js
@@ -0,0 +1,95 @@
+let actividades = [
+    {id:1 , nombre: 'Actividad 1', nota:0},
+    {id:2 , nombre: 'Actividad 2', nota:1.5},
+    {id:3 , nombre: 'Actividad 3', nota:5.0}
+];
+let indexActividadSeleccionada=-1;
+
+function mostrarMensaje(promedio){
+    let mensaje = promedio>=3?
+    'Aprobo la materia':
+    'Repite la materia';
+    document.getElementById('calificacion')
+    .innerText = mensaje;
+}
+
+function mostrarActividades(){
+    let filas = '';
+    let sumatoria = 0;
+    for(let index in actividades){
+        let actividad = actividades[index];
+        filas+= '<tr>';
+        filas+= '   <td>'+actividad.id+'</td>';
+        filas+= '   <td>'+actividad.nombre+'</td>';
+        filas+= '   <td>'+actividad.nota+'</td>';
+        filas+= '   <td>';
+        filas+= '   <button onclick="modificarActividad('+ index +')">Modificar</button>';
+        filas+= '   <button onclick="eliminarActividad(' + index + ')">Eliminar</button>';
+        filas+= '   </td>';
+        filas+= '</tr>';
+        sumatoria +=parseFloat(actividad.nota); 
+    }
+    let promedio = sumatoria/actividades.length;
+    document.getElementById('promedioTb')
+    .getElementsByTagName('tbody')[0]
+    .innerHTML=filas;
+
+    document.getElementById('promedioText')
+    .innerText = 'Su promedio es: '+promedio;  
+   
+    mostrarMensaje(promedio)
+}
+mostrarActividades();
+
+document.getElementById('crearBtn').addEventListener('click', ()=>{
+    indexActividadSeleccionada = -1;
+    document.getElementById('formActividad').reset();
+    document.getElementById('formularioModal')
+        .classList.remove('close-modal');
+        document.getElementById('crear').innerText='Crear';
+});
+
+document.getElementById('cerrar').addEventListener('click', ()=>{
+    document.getElementById('formularioModal')
+        .classList.add('close-modal');
+});
+
+document.getElementById('botonAceptar').addEventListener('click', ()=>{
+    let formulario = document.forms['formularioActividad'];
+    let actividad = formulario['actividad'].value;
+    let nota = formulario['nota'].value;
+
+    if(indexActividadSeleccionada==-1){
+
+    
+
+    let id = actividades.length + 1;//crear
+    actividades.push({
+        id: id,
+        nombre: actividad,
+        nota: nota
+    });//crear
+}else{
+    actividades[indexActividadSeleccionada].nombre= actividad;
+    actividades[indexActividadSeleccionada].nota= nota;
+}
+    mostrarActividades(); 
+    document.getElementById('formularioModal').
+    classList.add('close-modal');
+});
+
+
+function modificarActividad(posicionArray){
+    indexActividadSeleccionada =posicionArray;
+    document.getElementById('formularioModal')
+    .classList.remove('close-modal');
+    document.getElementById('crear').innerText='Modificar actividad';
+    let actividad = actividades [posicionArray];
+    document.getElementById('actividad').setAttribute('value', actividad.nombre);
+    document.getElementById('nota').setAttribute('value', actividad.nota);
+}
+
+function eliminarActividad(index){
+    actividades.splice(index, 1);
+    mostrarActividades();
+}
